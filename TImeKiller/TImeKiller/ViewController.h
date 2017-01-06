50236a85a6d7e82fbc6f33d242f32e4ad1953c89
jacob <het@ssiMac.local>
2017-01-06 12:05:22 +0800

    初始化项目

---
+++ TImeKiller/TImeKiller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeKiller
+//
+//  Created by 李明航 on 16/9/26.
+//  Copyright © 2016年 李明航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TImeKiller/TImeKiller/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimeKiller
+//
+//  Created by 李明航 on 16/9/26.
+//  Copyright © 2016年 李明航. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
